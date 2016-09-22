c50ebadc8bcd3017028fbb88a380babf6be0e954
dheeraj-gembali <insane710@gmail.com>
2016-09-23 01:24:10 +0530

    Second commit

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 var express = require('express');
 var app = express();
-app.use(express.static(__dirname + '/View'));
+app.use(express.static(__dirname + '/views'));
 //Store all HTML files in view folder.
-app.use(express.static(__dirname + '/Script'));
+app.use(express.static(__dirname + '/scripts'));
 //Store all JS and CSS in Scripts folder.
 
 app.get('/',function(req,res){
   res.sendFile('index.html');
-  //It will find and locate index.html from View or Scripts
+  //It will find and locate index.html from view or scripts
 });
 
 
 app.listen(8080, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('TEOPD started listening on port 8080');
 });
